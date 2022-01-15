28b1038efd1e9a83dfb558a5b04d2b5888aef86b
shouyamamoto <yamasyo0017@gmail.com>
2022-01-15 23:38:44 +0900

    practice fin 22

---
+++ Question22/index.js
@@ -0,0 +1,11 @@
+// 結果を教えろ
+let array = ['e', 'a', 'k', 'B', 'c'];
+// let array2 = [1, 'e', 'a', 'k', 'B', 'c'];
+// let array3 = ['あ', "い", 1, 'e', 'a', 'k', 'B', 'c'];
+array.sort();
+// array2.sort();
+// array3.sort();
+// console.log(array3)
+
+// 順番は文字エンコーディングによって決まる
+// 要素に数値があった場合文字列に置き換えられる
