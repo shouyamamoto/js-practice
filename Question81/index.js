88d9c2c54f3b5af8b33a35001ae24e8867730652
shouyamamoto <yamasyo0017@gmail.com>
2022-02-01 22:33:48 +0900

    feat: Question81

--- Question81/index.js
+++ Question81/index.js
@@ -1,13 +1,12 @@
 // 全てのaaにある多次元配列の全ての要素に文字列'san'を付け加えて一つの配列として出力してください
 // var aa = [['morita', 'kenji', 'keiko'],['morita', 'kenji', 'keiko']]
-
 const aa = [['morita', 'kenji', 'keiko'], ['morita', 'kenji', 'keiko']];
+const array = [];
 
-// sanを付け加える関数を実装
-const arry = aa.map(a => a.map(ele => `${ele}san`));
-
+// すべての要素にaddSan関数を実行する
+const executeAddSan = target => target.map(ele => addSan(ele));
+// sanを付ける関数を実装 
+const addSan = target => target.map(ele => `${ele}san`);
 // 2つの配列を1つの配列に変換　受け取った配列を1つの配列に順に格納していく関数
-const arryGroup = target => {
-}
-
-console.log(arryGroup(arry));
+executeAddSan(aa).forEach(ele => array.push(...ele));
+console.log(array);
