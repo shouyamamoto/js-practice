495741d29bf895a324edeb98f36acbf5936b3ab0
shouyamamoto <yamasyo0017@gmail.com>
2022-02-08 22:26:27 +0900

    feat: Question87

---
+++ Question87/index.js
@@ -0,0 +1,6 @@
+/**下記のようなURLのファイルパスごとに配列に格納してください
+ * https://github.com/kenmori/Angular2_TypeScript/tree/master/angular2-quickstart
+*/
+
+const URL = "https://github.com/kenmori/Angular2_TypeScript/tree/master/angular2-quickstart";
+const array = URL.split("/")
