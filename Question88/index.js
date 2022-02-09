ff0fde8a1be60d376b79159cca2c59c32664ef8e
shouyamamoto <yamasyo0017@gmail.com>
2022-02-09 23:46:23 +0900

    feat: Question88

---
+++ Question88/index.js
@@ -0,0 +1,14 @@
+/*
+下記のようなobj内のkeyと値が一緒の際できるshorthandで記述してください 
+*/
+
+const lukeSkywalker = 'Luke Skywalker';
+
+// bad
+const obj = {
+  lukeSkywalker: lukeSkywalker,
+};
+
+const obj2 = {
+  lukeSkywalker
+}
