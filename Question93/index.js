14c18907b0b92e591471e0a50535d34afb7206ad
shouyamamoto <yamasyo0017@gmail.com>
2022-02-18 23:32:51 +0900

    feat: Question93

---
+++ Question93/index.js
@@ -0,0 +1,9 @@
+/**
+ * var aaa = [['oo','oo1'], ['ll','ll2']];
+ * このような多次元配列のインデックス0番目だけを出力してください
+ */
+
+const arrays = [['oo', 'oo1'], ['ll', 'll2']];
+arrays.forEach(array => {
+  console.log(array[0])
+})
