c5feca987875e7756736dfdae27fdc0c2c8e7da3
shouyamamoto <yamasyo0017@gmail.com>
2022-02-25 23:54:27 +0900

    feat: question97

---
+++ Question97/index.js
@@ -0,0 +1,7 @@
+// var text = 'key and value';このような文字列を単語毎に配列の要素として格納してください 
+//期待する結果 
+//['key','and','value']
+
+const text = "key and value";
+const result = text.split(" ");
+console.log(result)
